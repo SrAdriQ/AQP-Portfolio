d56511e42a535231baa1bdab590dc19bf2740a3f
Adrián Quirós Pascual <adrianqsp@gmail.com>
2022-01-13 15:14:30 +0100

    Dev days 3

--- js/main.js
+++ js/main.js
@@ -1,5 +1,28 @@
-function closeWindow(containerId)
-{
+var isReady = 0;
+
+$(document).ready(function () {
+  load();
+});
+
+function load() {
+  if (isReady == 0) {
+    var currentProgress = 1;
+    var id = setInterval(frame, 5);
+
+    function frame() {
+      if (currentProgress >= 100) {
+        clearInterval(id);
+        isReady = 1;
+        closeWindow('load-screen')
+      } else {
+        currentProgress++;
+        $('#load-bar').width(currentProgress + "%");
+      }
+    }
+  }
+}
+
+function closeWindow(containerId) {
   $('#' + containerId).addClass('hide');
   $('#' + containerId).removeClass('show');
 }
