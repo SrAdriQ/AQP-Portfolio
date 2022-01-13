3b7c8bf0f67964360ac05346dca0647192348cce
adrianqsp <adrianqsp@gmail.com>
2022-01-13 18:45:24 +0100

    Dev day 4

--- js/main.js
+++ js/main.js
@@ -4,6 +4,12 @@ $(document).ready(function () {
   load();
 });
 
+$(window).scroll(function() {
+  if ($('#jobs').hasClass('show')) {
+    fadeIn();
+  }
+});
+
 function load() {
   if (isReady == 0) {
     var currentProgress = 1;
@@ -31,3 +37,53 @@ function openWindow(containerId) {
   $('#' + containerId).addClass('show');
   $('#' + containerId).removeClass('hide');
 }
+
+function fadeIn(){
+
+  window.sr = ScrollReveal();
+
+  if ($(window).width() < 768) {
+
+  	if ($('.timeline-card').hasClass('fadeInLeft')) {
+  		$('.timeline-card').removeClass('fadeInLeft').addClass('fadeInRight');
+  	}
+
+  	sr.reveal('.fadeInRight', {
+	    origin: 'right',
+	    distance: '300px',
+	    easing: 'ease-in-out',
+	    duration: 800,
+	  });
+
+  } else {
+  	
+  	sr.reveal('.fadeInLeft', {
+	    origin: 'left',
+	    distance: '300px',
+		  easing: 'ease-in-out',
+	    duration: 800,
+	  });
+
+	  sr.reveal('.fadeInRight', {
+	    origin: 'right',
+	    distance: '300px',
+	    easing: 'ease-in-out',
+	    duration: 800,
+	  });
+
+  }
+  
+  sr.reveal('.fadeInLeft', {
+	    origin: 'left',
+	    distance: '300px',
+		  easing: 'ease-in-out',
+	    duration: 800,
+	  });
+
+	  sr.reveal('.fadeInRight', {
+	    origin: 'right',
+	    distance: '300px',
+	    easing: 'ease-in-out',
+	    duration: 800,
+	  });
+  }
